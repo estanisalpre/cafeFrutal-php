229b8690c1a840d07be6a18d927c22726398d04c
Estanislao Salinas <estanisprevite@hotmail.com>
2024-11-14 17:29:31 -0500

    Creamos rutas con composer y flight

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php
-require 'vendor/autoload.php';
+// Cargar Composer
+require '../vendor/autoload.php';
 
+// Configurar Flight para que use 'views' como directorio de plantillas
+Flight::set('flight.views.path', '../views');
+
+// Ruta para la página de inicio
 Flight::route('/', function(){
-    echo '¡Hola, bienvenido a mi sitio web!';
+    Flight::render('index');  // Carga views/index.php
 });
 
+// Ruta para la sección de administración
+Flight::route('/admin', function(){
+    Flight::render('admin');  // Carga views/admin.php
+});
+
+// Ruta para la sección de productos
+Flight::route('/productos', function(){
+    Flight::render('productos');  // Carga views/productos.php
+});
+
+// Iniciar Flight
 Flight::start();
