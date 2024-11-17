857034dfd74c33968c7de6717eef1be24406516c
Estanislao Salinas <estanisprevite@hotmail.com>
2024-11-16 21:02:59 -0500

    Funcionalidad de ver lista en admin.php, editar y eliminar botones

--- delete_product.php
+++ delete_product.php
@@ -1,4 +1,8 @@
 <?php
+
+// Incluir el archivo de conexión a la base de datos
+include 'db.php';
+
     header('Content-Type: application/json');
     $data = json_decode(file_get_contents('php://input'), true);
 
@@ -8,7 +12,6 @@
     }
 
     try {
-        $pdo = new PDO('mysql:host=82.197.82.73;dbname=u584871321_stockProducts', 'u584871321_admin', 'Kinesis1233');
         $stmt = $pdo->prepare("DELETE FROM products WHERE id = :id");
         $stmt->bindParam(':id', $data['id'], PDO::PARAM_INT);
         $success = $stmt->execute();
